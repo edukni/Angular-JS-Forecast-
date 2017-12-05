5fd2e056fb2b521712bf76ec56f5c047089e5006
edukni <eduk_1012@hotmail.com>
2017-12-05 17:58:11 -0600

    Controllers and custom service

--- app.js
+++ app.js
@@ -1,2 +1,38 @@
 
-var weatherApp= angular.module('weatherApp',[]);
+
+//MODULE
+var weatherApp= angular.module('weatherApp',['ngRoute','ngResource']);
+
+weatherApp.config(function($routeProvider){
+
+	$routeProvider
+	.when('/',{
+		templateUrl: 'pages/home.html',
+		controller: 'homeController'
+	})
+	.when('/forecast',{
+		templateUrl: 'pages/forecast.html',
+		controller: 'forecastController'
+	})
+});
+
+
+//SERVICES 
+weatherApp.service('cityService', function(){
+	this.city="New York, NY"
+});
+
+//CONTROLLERS
+weatherApp.controller('homeController', ['$scope','cityService', function($scope, cityService){ 
+
+	$scope.city= cityService.city;
+	$scope.$watch('city', function(){
+		cityService.city=$scope.city;
+	});
+}]);
+
+weatherApp.controller('forecastController', ['$scope','cityService', function($scope, cityService){ 
+
+	$scope.city= cityService.city;
+
+}]);
