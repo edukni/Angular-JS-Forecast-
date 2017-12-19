dfd5bcd6e797020e2683390113ce7def7108937c
edukni <eduk_1012@hotmail.com>
2017-12-19 12:19:26 -0600

    Binding data part 2

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ weatherApp.controller('homeController', ['$scope','cityService', function($scope
 
 weatherApp.controller('forecastController', ['$scope','$resource','$routeParams','cityService', function($scope, $resource, $routeParams, cityService){ 
 	$scope.city = cityService.city;
-	$scope.days = $routeParams.days || 2;
+	$scope.days = $routeParams.days || '2';
 	$scope.weatherAPI=$resource("http://api.openweathermap.org/data/2.5/find?appid=25fd96f0394bcace50a44d4439853f93");
 	$scope.weatherResult = $scope.weatherAPI.get({q:$scope.city, cnt:$scope.days });
 	$scope.convertToCelsius = function(degK){
